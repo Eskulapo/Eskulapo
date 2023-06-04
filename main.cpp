43ff80fd2c0dc6f88d623c8466e4054fdd3ea5c4
Eskulapo <92510686+Eskulapo@users.noreply.github.com>
2023-06-04 15:11:34 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
+
+void numerowanie(std::string s)
+{
+    std::string wyjscie;
+    int n = 2;
+    wyjscie+="1 ";
+    for(int i = 0; i < s.length(); i++)
+    {
+        wyjscie+=s[i];
+        if(s[i] == '\n'){
+            s.insert(i, std::to_string(n)+" ");
+            n++;
+        }
+    }
+}
 
 int main()
 {
-    
+    std::string buffor;
+    std::cin>>buffor;
+
     return 0;
 }
