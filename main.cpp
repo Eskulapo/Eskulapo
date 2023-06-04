15658a2e34e8aea322a9b0e5c0be3dc2528b3853
Eskulapo <92510686+Eskulapo@users.noreply.github.com>
2023-06-04 16:08:55 +0200

    3

--- main.cpp
+++ main.cpp
@@ -5,21 +5,31 @@ void numerowanie(std::string s)
 {
     std::string wyjscie;
     int n = 2;
-    wyjscie+="1 ";
-    for(int i = 0; i < s.length(); i++)
+    wyjscie += "1 ";
+    //std::cout << s.length();
+    for (int i = 0; i < s.length(); i++)
     {
-        wyjscie+=s[i];
-        if(s[i] == '\n'){
-            s.insert(i, std::to_string(n)+" ");
+        wyjscie += s[i];
+        if (s[i] == '\n')
+        {
+            s.insert(i, std::to_string(n) + " ");
             n++;
         }
     }
+    std::cout<<wyjscie<<std::endl;
 }
 
 int main()
 {
-    std::string buffor;
-    std::cin>>buffor;
+    std::string buffor, input;
+    do{
+        std::getline(std::cin, buffor);
+        input+=buffor;
+        input+='\n';
+    }
+    while(!buffor.empty());
+    //std::cout<<buffor;
+    numerowanie(input);
 
     return 0;
 }
